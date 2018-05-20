a3425a5ebb610ab75913013f10dcb61b9f982757
ishida <webmaster@joe1cafe.com>
2018-05-20 15:49:48 +0800

    feat(beauty): add batch file to create thumbnails for existing images

---
+++ compress.bat
@@ -0,0 +1,11 @@
+@echo off 
+set work_path=C:\Users\Administrator\Desktop\wechat\uploads 
+C: 
+cd %work_path% 
+for /f %%I in ('dir /a /b /s /od *') do (
+::for /R %%I in (*) do ( 
+echo %%~dpI%%~nxI
+::echo %%~tI^|asd^|%%~nxI^|%%~dpI
+convert %%~dpI%%~nxI -resize 180x180 %%~dpIthumbs\thumb_%%~nxI
+) 
+pause 
